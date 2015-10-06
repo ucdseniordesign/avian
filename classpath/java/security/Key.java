2e196844d207f15b4b901e47dc08f9dc126062e6
long <long.huynh@ucdenver.edu>
2015-10-06 15:20:04 -0600

    adding interfaces for X509KeyManager, Key and PrivateKey

---
+++ classpath/java/security/Key.java
@@ -0,0 +1,8 @@
+package java.security;
+
+import java.io.Serializable;
+
+public interface Key extends Serializable {
+
+	static final long serialVersionUID = 6603384152749567654L;
+}
--- classpath/javax/net/ssl/X509KeyManager.java
+++ classpath/javax/net/ssl/X509KeyManager.java
@@ -3,13 +3,19 @@ package javax.net.ssl;
 import java.security.cert.X509Certificate;
 import java.security.Principal;
 import java.net.Socket;
+import java.security.PrivateKey;
 public interface X509KeyManager extends KeyManager {
 
 String chooseClientAlias(String[] keyType, Principal[] issuers, Socket socket);
+
 String chooseServerAlias(String keyType, Principal[] issuers, Socket socket);
+
 X509Certificate[] getCertificateChain(String alias);
+
 String[] getClientAliases(String keyType, Principal[] issuers);
+
 PrivateKey getPrivateKey(String alias);
+
 String[] getServerAliases(String keyType, Principal[] issuers);
 
 }
