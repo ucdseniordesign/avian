8261fdcd99c3134ef1e8315386a0f1b4c4c63e00
long <long.huynh@ucdenver.edu>
2015-10-20 14:54:43 -0600

    adding new comment and code style

--- classpath/java/security/Key.java
+++ classpath/java/security/Key.java
@@ -1,8 +1,10 @@
 package java.security;
 
+//import the library for Key class
 import java.io.Serializable;
 
 public interface Key extends Serializable {
 
+	// get the serialVersionUID number from http://docs.oracle.com/javase/6/docs/api/constant-values.html#java.security.Key.serialVersionUID
 	static final long serialVersionUID = 6603384152749567654L;
 }
--- classpath/java/security/PrivateKey.java
+++ classpath/java/security/PrivateKey.java
@@ -1,9 +1,11 @@
 package java.security;
 
+//import library for PrivateKey class
 import java.security.Key;
 
 public interface PrivateKey extends Key {
 
+	// get the serialVersionUID from http://docs.oracle.com/javase/6/docs/api/constant-values.html#java.security.Key.serialVersionUID
 	static final long serialVersionUID = 6034044314589513430L;
 
 }
