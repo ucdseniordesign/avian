63ed1b9e8fbb1d6fd012f12e4f0676407833b642
Jason Minor <minor.jason@gmail.com>
2015-10-20 14:19:35 -0600

    Initial commit of SSLParameters.java

--- classpath/javax/net/ssl/SSLParameters.java
+++ classpath/javax/net/ssl/SSLParameters.java
@@ -2,21 +2,59 @@ package javax.net.ssl;
 
 import java.security.AlgorithmConstraints;
 
-public class SSLParameters extends Object {
-    public SSLParameters();
-    public SSLParameters(String[] cipherSuites);
-    public SSLParameters(String[] cipherSuites, String[] protocols);
+public class SSLParameters {
+
+    private String[] cipherSuites;
+    private String[] protocols;
+    private boolean wantClientAuth;
+    private boolean needClientAuth;
+    private String identificationAlgorithm;
+    private AlgorithmConstraints algorithmConstraints;
+
+    public SSLParameters(){}
+    public SSLParameters(String[] cipherSuites) {
+        setCipherSuites(cipherSuites);
+    }
+    public SSLParameters(String[] cipherSuites, String[] protocols) {
+        setCipherSuites(cipherSuites);
+        setProtocols(protocols);
+    }    
     
-    AlgorithmConstraints getAlgorithmConstraints();
-    String[] getCipherSuites();
-    String getEndpointIdentificationAlgorithm();
-    boolean GetNeedClientAuth();
-    String[] getProtocols();
-    boolean getWantClientAuth();
-    void setAlgorithmConstraints(AlgorithmConstraints constraints);
-    void setCipherSuites(String[] cipherSuites);
-    void setEndpointIdntificationAlgorithm(String algorithm);
-    void setNeedClientAuth(boolean needClientAuth);
-    void setProtocols(String[] protocols);
-    void setWantClientAuth(boolean wantClientAuth);
+    AlgorithmConstraints getAlgorithmConstraints() {
+        return algorithmConstraints;
+    }
+    String[] getCipherSuites() {
+        return cipherSuites;
+    }
+    String getEndpointIdentificationAlgorithm() {
+        return identificationAlgorithm;
+    }
+    boolean getNeedClientAuth() {
+        return needClientAuth;
+    }
+    String[] getProtocols() {
+        return protocols;
+    }
+    boolean getWantClientAuth() {
+        return wantClientAuth;
+    }
+
+    void setAlgorithmConstraints(AlgorithmConstraints constraints) {
+        this.algorithmConstraints = constraints;
+    }
+    void setCipherSuites(String[] cipherSuites) {
+        this.cipherSuites = cipherSuites;
+    }
+    void setEndpointIdntificationAlgorithm(String algorithm) {
+        this.identificationAlgorithm = algorithm;
+    }
+    void setNeedClientAuth(boolean needClientAuth) {
+        this.needClientAuth = needClientAuth;
+    }
+    void setProtocols(String[] protocols) {
+        this.protocols = protocols;
+    }
+    void setWantClientAuth(boolean wantClientAuth) {
+        this.wantClientAuth = wantClientAuth;
+    }
 }
