312adf0d22061232b26978e163df323f2510f739
Jason Minor <minor.jason@gmail.com>
2015-11-05 13:53:36 -0700

    errors recognizing openssl library

--- classpath/javax-net-ssl.cpp
+++ classpath/javax-net-ssl.cpp
@@ -1,11 +1,22 @@
 // javax-net-ssl.cpp
 #include "jni.h"
 #include "jni-util.h"
+#include <openssl/ssl.h>
+#include <openssl/err.h>
 
-#include "sslcontext.h"
-using namespace avian::classpath::sslcontext;
-extern "C" JNIEXPORT SSLCONTEXT JNICALL Java_javax_net_ssl_SSLContext_create(JNIEnv* env, jclass) {
-    return create();
+#define SSLCONTEXT jlong
+
+SSL_library_init();
+
+extern "C" JNIEXPORT SSLCONTEXT JNICALL Java_javax_net_ssl_SSLContext_create(/*JNIEnv* env, jclass*/) {
+    //SSL_library_init();
+    SSL_CTX* ctx = SSL_CTX_new(SSLv23_method());
+
+    if(ctx == NULL) {
+        printf("Error making context\n");
+        exit(1);
+    }
+    return (jlong)ctx;
 }/*
 extern "C" JNIEXPORT void JNICALL Java_javax_net_ssl_SSLContext_init(JNIEnv* env, jclass) {
     init(env);
