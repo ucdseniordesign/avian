90ac5cdb92b56c5d0ab6ffaacea75378c87a14ce
Jason Minor <minor.jason@gmail.com>
2016-02-16 14:26:36 -0700

    cleaned up the two test files for SSLengine and SSLcontext. Both tests require a test.pem file. An environment variable might be handy until we get KeyStore working.

--- test/SSLContextTest.java
+++ test/SSLContextTest.java
@@ -5,32 +5,11 @@ import java.nio.ByteBuffer;
 public class SSLContextTest {
 
     public static void main(String[] args) {
-        System.out.println("---------SSL----------");
+        System.out.println("---------SSLContext----------");
         SSLContext sslCtx = SSLContext.getInstance("SSLv3");
-        sslCtx.loadPemCertAndKey("/home/jason/sample/test.pem", "/home/jason/sample/test.pem");
+        sslCtx.loadPemCertAndKey("/home/jason/AvianSSLsample/test.pem", "/home/jason/AvianSSLsample/test.pem");
         SSLEngine sslEng = sslCtx.createSSLEngine();
-
-        String plaintxt = "Hello World";
-        ByteBuffer ptBB = ByteBuffer.wrap(plaintxt.getBytes());
-        ByteBuffer ctBB = ByteBuffer.allocateDirect(ptBB.capacity());
-        byte[] ptArr = new byte[ptBB.remaining()];
-       
-        sslEng.wrap(ptBB, ctBB);
-        ptBB.clear();
-        sslEng.unwrap(ctBB, ptBB);
-
-        /*
-        byte[] ptArr = new byte[ptBB.remaining()];
-        ptBB.get(ptArr);
-        String plaintxt_ = new String(ptArr);
-        System.out.println(plaintxt_);
-
-        byte[] ctArr = new byte[ctBB.remaining()];
-        ctBB.get(ctArr);
-        String ciphertxt = new String(ctArr);
-        System.out.println(ciphertxt);
-        */
-        System.out.println("---------SSL----------");
+        System.out.println("---------SSLContext----------");
         
     }
 }
