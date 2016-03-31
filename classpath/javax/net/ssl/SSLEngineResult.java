fd0a191957a0c454a844bf852ea477b3d047ce9c
Jason Minor <minor.jason@gmail.com>
2016-03-31 13:46:43 -0600

    Added more methods to SSLEngineResult

--- classpath/javax/net/ssl/SSLEngineResult.java
+++ classpath/javax/net/ssl/SSLEngineResult.java
@@ -1,6 +1,7 @@
 package javax.net.ssl;
 
 public class SSLEngineResult {
+
     public static enum HandshakeStatus {
         FINISHED,
         NEED_TASK,
@@ -8,4 +9,41 @@ public class SSLEngineResult {
         NEED_WRAP,
         NOT_HANDSHAKING
     }
+    public static enum Status {
+    	BUFFER_OVERFLOW,
+    	BUFFER_UNDERFLOW,
+    	CLOSED,
+    	OK
+    }
+
+    private final int _bytesConsumed;
+	private final int _bytesProduced;
+	private final HandshakeStatus _hsStatus;
+	private final Status _status;
+
+    public SSLEngineResult(SSLEngineResult.Status status,
+    		SSLEngineResult.HandshakeStatus hsStatus,
+    		int bytesConsumed, int bytesProduced) {
+    	_bytesConsumed = bytesConsumed;
+    	_bytesProduced = bytesProduced;
+    	_hsStatus = hsStatus;
+    	_status = status;
+    }
+
+    public final int bytesConsumed() {
+    	return _bytesConsumed;
+    }
+
+    public final int bytesProduced() {
+    	return _bytesProduced;
+    }
+
+    public final SSLEngineResult.HandshakeStatus getHandshakeStatus() {
+    	return _hsStatus;
+    }
+
+    public final SSLEngineResult.Status getStatus() {
+    	return _status;
+    }
+
 }
